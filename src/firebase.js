f4a39c818d9ec9628169076f0a4948af17adef91
Héctor Espinoza <hespinoza@uchile.cl>
2020-09-02 14:15:33 -0400

    added .env.local

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCIy2OQYs1ICkThbNDwqkU_kPdf1CwKVGM",
+    apiKey: `${process.env.FIREBASE_KEY}`,
     authDomain: "iamamitito-library.firebaseapp.com",
     databaseURL: "https://iamamitito-library.firebaseio.com",
     projectId: "iamamitito-library",
     storageBucket: "iamamitito-library.appspot.com",
     messagingSenderId: "545859090063",
-    appId: "1:545859090063:web:1483a734c50e6dfa85e2fa",
+    appId: `${process.env.FIREBASE_ID}`,
     measurementId: "G-YY13SXK8M5"
 });
 
